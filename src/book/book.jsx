ff4215050018b303dca6b3cf3067af793914e484
Asomiddin <asomiddin320@gmail.com>
2024-05-25 15:43:45 +0500

    responsive

--- src/book/book.jsx
+++ src/book/book.jsx
@@ -8,37 +8,37 @@ const Book = () => {
         <img id='logo' className='w-[50px] duration-75 hover:scale-[120%]' src={require('../logos/bookworm_emblem_black.png')} alt="logo" />
       </Link>
 
-      <div className='w-full'>
-        <div className='shad_ui max-w-[500px]  rounded-[20px] mx-auto my-10  py-10 px-1 sm:px-6'>
+      <div className='w-full pt-5'>
+        <div className='shad_ui max-w-[500px]  rounded-[20px] mx-auto my-10 py-6 sm:py-10 px-1 sm:px-6'>
           <div className='flex gap-2 px-1'>
             <div className='flex justify-start w-[49%]'>
               <img className='w-[200px] h-[270px] rounded-[15px]' src="https://assets.asaxiy.uz/product/items/desktop/df763d665084612d719bfaf359cc6bb32021040216281526613clrASkcKsR.jpg.webp" alt="book" />
             </div>
 
             <div className='w-[49%] '>
-              <h1 className='text-[18px] font-semibold pt-1'>Лев Толстой: Уруш ва тинчлик (3-4 китоблар)</h1>
-              <p className='py-2 text-[14px]'>Характеристики</p>
-              <section className='flex justify-between pt-1 px-1 text-[14px] border-b-2 text-[#443f3f] border-[#535050]  border-dotted'>
+              <h1 className='text-[16px] sm:text-[18px] font-semibold pt-1 '>Лев Толстой: Уруш ва тинчлик (3-4 китоблар)</h1>
+              <p className='py-2  text-[13px] sm:text-[14px]'>Характеристики</p>
+              <section className='flex justify-between pt-1 px-1 text-[12px]  sm:text-[14px] border-b-2 text-[#443f3f] border-[#535050]  border-dotted'>
                 <p>ISBN</p>
                 <p>978-9943-5367-8-4</p>
               </section>
-              <section className='flex justify-between pt-1 px-1 text-[14px] border-b-2 text-[#443f3f] border-[#535050]  border-dotted'>
+              <section className='flex justify-between pt-1 px-1 text-[12px]  sm:text-[14px] border-b-2 text-[#443f3f] border-[#535050]  border-dotted'>
                 <p>Автор</p>
                 <p>Л. Н.Толстой</p>
               </section>
-              <section className='flex justify-between pt-1 px-1 text-[14px] border-b-2 text-[#443f3f] border-[#535050]  border-dotted'>
+              <section className='flex justify-between pt-1 px-1 text-[12px]  sm:text-[14px] border-b-2 text-[#443f3f] border-[#535050]  border-dotted'>
                 <p>Язык</p>
                 <p>На узбекском</p>
               </section>
-              <section className='flex justify-between pt-1 px-1 text-[14px] border-b-2 text-[#443f3f] border-[#535050]  border-dotted'>
+              <section className='flex justify-between pt-1 px-1 text-[12px]  sm:text-[14px] border-b-2 text-[#443f3f] border-[#535050]  border-dotted'>
                 <p>Надпись</p>
                 <p>Кириллица</p>
               </section>
-              <section className='flex justify-between pt-1 px-1 text-[14px] border-b-2 text-[#443f3f] border-[#535050]  border-dotted'>
+              <section className='flex justify-between pt-1 px-1 text-[12px]  sm:text-[14px] border-b-2 text-[#443f3f] border-[#535050]  border-dotted'>
                <p>Количество страниц</p>
                 <p>928</p>
               </section>
-             <section className='flex justify-between pt-1 px-1 text-[14px] border-b-2 text-[#443f3f] border-[#535050]  border-dotted'>
+             <section className='flex justify-between pt-1 px-1 text-[12px]  sm:text-[14px] border-b-2 text-[#443f3f] border-[#535050]  border-dotted'>
               <p>Год издания</p>
                 <p>2019</p>
               </section>
